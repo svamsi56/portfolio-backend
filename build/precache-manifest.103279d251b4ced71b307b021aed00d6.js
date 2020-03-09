c4e8502aa4cde7a2c9fbf707b80f25bf7658cbd9
svamsi56 <svkirshna56@gmail.com>
2020-03-09 00:32:24 -0400

    Initial commit for my portfolio website!

---
+++ build/precache-manifest.103279d251b4ced71b307b021aed00d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c13a182ee818b6e3fb34ec21bfa6bd2b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1422ae9335927c9f72b7",
+    "url": "/static/css/main.6e8f668b.chunk.css"
+  },
+  {
+    "revision": "b46f93687afe6f07861a",
+    "url": "/static/js/2.58af80b3.chunk.js"
+  },
+  {
+    "revision": "f231859d6585c4cd5f80c344783ed269",
+    "url": "/static/js/2.58af80b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1422ae9335927c9f72b7",
+    "url": "/static/js/main.c76fa928.chunk.js"
+  },
+  {
+    "revision": "2b748575d325adc11ce4",
+    "url": "/static/js/runtime-main.493d5184.js"
+  },
+  {
+    "revision": "a9592d41c6c2bf738a6fbff4514dbd9f",
+    "url": "/static/media/bgImage.a9592d41.jpg"
+  }
+]);
